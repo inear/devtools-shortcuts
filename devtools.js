b946626de7c11c3ca502b327887fb44003fff00c
Einar Öberg <einar@northkingdom.com>
2014-01-11 22:29:26 +0100

    Fix so just 'inspect' works as inspect='true'

--- devtools.js
+++ devtools.js
@@ -5,15 +5,25 @@
 // Create a tab in the devtools area
 
 var page_getShortcuts = function() {
-  var data = document.querySelectorAll('[inspect=\"true\"]');
-  return data;
+
+  var data = document.querySelectorAll('[inspect]');
+  var matchingElements = [];
+
+  for (var i = data.length - 1; i >= 0; i--) {
+    var value = data[i].getAttribute('inspect').toString();
+    if( value === 'true' || value === '' )  {
+      matchingElements.unshift(data[i]);
+    }
+  }
+
+  return matchingElements;
 };
 
 chrome.devtools.panels.elements.createSidebarPane(
   "Shortcuts",
   function(sidebar) {
     function updateElementProperties() {
-      sidebar.setExpression("(" + page_getShortcuts.toString() + ")()");
+      sidebar.setExpression("(" + page_getShortcuts + ")()");
     }
     updateElementProperties();
     chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
